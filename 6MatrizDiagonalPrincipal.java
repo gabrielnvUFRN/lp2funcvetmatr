c02008f813b3f53774c53d137b7a91df6033fedc
gabrielnvUFRN <gabriel.vieira.084@ufrn.edu.br>
2024-10-21 13:51:34 -0300

    Create 6MatrizDiagonalPrincipal.java

---
+++ 6MatrizDiagonalPrincipal.java
@@ -0,0 +1,24 @@
+public class MatrizDiagonalPrincipal {
+
+    public static void main(String[] args) {
+        int[][] matriz = new int[5][5];
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (i == j) {
+                    matriz[i][j] = 1;
+                } else {
+                    matriz[i][j] = 0;
+                }
+            }
+        }
+
+        System.out.println("Matriz 5x5:");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
